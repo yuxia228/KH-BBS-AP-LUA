40aa4113e8bd2ea60886a3891ce28d2e832f17a6
gaithern <gaithern@gmail.com>
2024-07-22 11:34:46 -0500

    Update bbsAPConnector.lua

--- bbsAPConnector.lua
+++ bbsAPConnector.lua
@@ -88,7 +88,7 @@ function define_world_progress_location_bits()
     --Deep Space
     world_progress_location_bits[1][9][10]  = {2271120800} --Air Slide
     world_progress_location_bits[1][9][11]  = {2271120801} --Max HP Increase
-    world_progress_location_bits[1][9][16]  = {2271120802, 2271120803, 2271120804, 2271120805} --Deck Capacity Increase, Experiment 626 D-Link, Hyperdrive, Spaceship Board
+    world_progress_location_bits[1][9][15]  = {2271120802, 2271120803, 2271120804, 2271120805} --Deck Capacity Increase, Experiment 626 D-Link, Hyperdrive, Spaceship Board
     --Neverland
     world_progress_location_bits[1][11][6]  = {2271121000} --Doubleflight
     world_progress_location_bits[1][11][8]  = {2271121001, 2271121002} --Max HP Increase, Ghost Drive
